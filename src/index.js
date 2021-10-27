d7ecec6d1f855ab337cac412a1843eb2f3e694f4
Lidija Levchenko <levchenkola@ukr.net>
2021-10-27 22:40:33 +0300

    +

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('module 1.1');
+function getExtremeElements(array) {
+    // Change code below this line
+  //result = [array[0], array[array.length - 1]]
+      // Change code above this line
+  }
+  const brand = 'samsung';
+  console.log(brand[0]);
+  jhugygy
