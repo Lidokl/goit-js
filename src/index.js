af9993329d2125d491d9e0b261097da9a26e9420
Lidija Levchenko <levchenkola@ukr.net>
2021-11-05 23:08:32 +0200

    k

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 console.log('module 1.1');
-function getExtremeElements(array) {
+
+const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+  
+  function getProductPrice(productName) {
     // Change code below this line
-  //result = [array[0], array[array.length - 1]]
-      // Change code above this line
-  }
-  const brand = 'samsung';
-  console.log(brand[0]);
-  jhugygy
+ }
